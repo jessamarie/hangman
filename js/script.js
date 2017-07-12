5a695bac67a06b36013c5eec119eb4cbdf9827f7
Jessa Marie Barre <jmlbarre@gmail.com>
2017-07-11 21:26:27 -0400

    created basic structure and styling

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Have a player enter a word that will be guessed during the game.
+// The word is then hidden and represented by blank spaces.
+// The second player then chooses letters, which are revealed if present.
+//
+// Bonus:
+//
+// Add timer-based scoring
+// Track scores across games (even if the page is reloaded)
+
+
+/* global $ */
+console.log('get coding!')
+
+setTimeout(function() {
+
+//  $('#welcome').addClass('hide')
+//   $('#game').removeClass('hide')
+
+},100);
+
+ $('#welcome').addClass('hide')
+ $('#game').removeClass('hide')
