f15ba6110c56b41f34c4e9689e4025f567e7b6e9
Jessa Marie Barre <jmlbarre@gmail.com>
2017-07-14 10:35:28 -0400

    made letters only clickable once

--- js/WindowView.js
+++ js/WindowView.js
@@ -60,6 +60,9 @@ class WindowView {
 
   handleLetterClick (e) {
     $(e.target).css('color', 'gray')
+    $(e.target).css('pointer-events', 'none')
+
+    console.log('Im here')
 
     if (this.model.checkGuess(e.target.outerText)) {
       this.renderGuesses()
@@ -74,36 +77,38 @@ class WindowView {
       if (this.model.lostGame()) {
         $('audio').trigger('pause').prop('currentTime', 0)
         $('.decapitation').trigger('play')
-
       } else {
         $('audio').trigger('pause').prop('currentTime', 0)
         $('.torture').trigger('play')
-
       }
     }
   }
 
   handleWordEntry (e) {
     if (e.which === 13) {
+      if (this.input.val() !== '') {
       // TODO: error check if val() === empty
-      e.preventDefault()
-      this.displays.word.addClass('hide')
-      this.displays.game.removeClass('hide')
+        e.preventDefault()
+        this.displays.word.addClass('hide')
+        this.displays.game.removeClass('hide')
 
       /* TODO: make letters clickable */
-      this.displays.letter.css('color', 'black')
+        this.displays.letter.css('color', 'black')
 
-      this.model.setWord(this.input.val())
-      this.input.val('') // clears input
+        this.model.setWord(this.input.val())
+        this.input.val('') // clears input
 
-      this.renderGuesses()
-      this.hangman.resizeCanvas()
+        this.renderGuesses()
+        this.hangman.resizeCanvas()
+      }
     }
   }
 
   playGame () {
     /* TODO: make letters unclickable */
     this.displays.letter.css('color', 'black')
+    this.displays.letter.css('pointer-events', 'auto')
+
     this.displays.welcome.addClass('hide')
     this.displays.game.addClass('hide')
     this.displays.word.removeClass('hide')
